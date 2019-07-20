6a1af11d912fcc6e62556bc171d1005471e36392
Nicholas Henry <nicholas.henry@gmail.com>
2019-07-20 11:56:59 -0400

    feat: Getting vital force

--- lib/frankenfunctor.ex
+++ lib/frankenfunctor.ex
@@ -1,2 +1,23 @@
 defmodule Frankenfunctor do
+  defmodule VitalForce do
+    @type t :: {:units, integer}
+
+    @spec new(integer) :: t
+    def new(value), do: {:units, value}
+
+    @spec get_vital_force(t) :: {t, t}
+    def get_vital_force(vital_source) do
+      one_unit = new(1)
+
+      {
+        one_unit,
+        sub(vital_source, one_unit)
+      }
+    end
+
+    @spec sub(t, t) :: t
+    def sub({:units, lhs}, {:units, rhs}) do
+      new(lhs - rhs)
+    end
+  end
 end
--- test/frankenfunctor_test.exs
+++ test/frankenfunctor_test.exs
@@ -1,3 +1,14 @@
 defmodule FrankenfunctorTest do
   use ExUnit.Case
+
+  alias Frankenfunctor.VitalForce
+
+  test "getting vital force" do
+    vital_force = VitalForce.new(10)
+
+    {actual_vital_force, remaining_vital_force} = VitalForce.get_vital_force(vital_force)
+
+    assert actual_vital_force == VitalForce.new(1)
+    assert remaining_vital_force == VitalForce.new(9)
+  end
 end
