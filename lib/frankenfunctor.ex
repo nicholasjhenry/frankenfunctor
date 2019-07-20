5dfaf2f1587247d725df098c2ae521958daf777d
Nicholas Henry <nicholas.henry@gmail.com>
2019-07-20 12:24:46 -0400

    feat: Left leg

--- lib/frankenfunctor.ex
+++ lib/frankenfunctor.ex
@@ -20,4 +20,45 @@ defmodule Frankenfunctor do
       new(lhs - rhs)
     end
   end
+
+  defmodule Label do
+    @type t :: String.t()
+  end
+
+  defmodule DeadLeftLeg do
+    @type t :: {:dead_left_leg, Label.t()}
+
+    @spec new(Label.t()) :: t
+    def new(label), do: {:dead_left_leg, label}
+  end
+
+  defmodule LiveLeftLeg do
+    @type t :: {{:live_left_leg, Label.t()}, VitalForce.t()}
+
+    @spec new(Label.t(), VitalForce.t()) :: t
+    def new(label, vital_force) do
+      {{:live_left_leg, label}, vital_force}
+    end
+  end
+
+  defmodule M do
+    @type t(live_body_part) :: {:m, (VitalForce.t() -> {live_body_part, VitalForce})}
+
+    def new(func) do
+      {:m, func}
+    end
+  end
+
+  @spec make_live_left_leg_m(DeadLeftLeg.t()) :: M.t(LiveLeftLeg.t())
+  def make_live_left_leg_m(dead_left_leg) do
+    become_alive = fn vital_force ->
+      {:dead_left_leg, label} = dead_left_leg
+      {one_unit, remain_vital_force} = VitalForce.get_vital_force(vital_force)
+      live_left_leg = LiveLeftLeg.new(label, one_unit)
+
+      {live_left_leg, remain_vital_force}
+    end
+
+    M.new(become_alive)
+  end
 end
--- test/frankenfunctor_test.exs
+++ test/frankenfunctor_test.exs
@@ -1,7 +1,9 @@
 defmodule FrankenfunctorTest do
   use ExUnit.Case
 
-  alias Frankenfunctor.VitalForce
+  alias Frankenfunctor.{DeadLeftLeg, LiveLeftLeg, VitalForce}
+
+  import Frankenfunctor
 
   test "getting vital force" do
     vital_force = VitalForce.new(10)
@@ -10,5 +12,16 @@ defmodule FrankenfunctorTest do
 
     assert actual_vital_force == VitalForce.new(1)
     assert remaining_vital_force == VitalForce.new(9)
+  end
+
+  test "left leg" do
+    dead_left_leg = DeadLeftLeg.new("Boris")
+    left_leg_m = make_live_left_leg_m(dead_left_leg)
+    vf = VitalForce.new(10)
+    {:m, inner_fn} = left_leg_m
+    {live_left_leg, remaining_after_left_leg} = inner_fn.(vf)
+
+    assert live_left_leg == LiveLeftLeg.new("Boris", VitalForce.new(1))
+    assert remaining_after_left_leg == VitalForce.new(9)
   end
 end
